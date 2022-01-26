147795ba6b013206670ea76ed639f423f3dcb4d4
Jillium <86424914+Jillium@users.noreply.github.com>
2022-01-26 10:20:21 -0700

    modal functionality completed

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Modal({currentPhoto}) {
+function Modal({ onClose, currentPhoto}) {
     const {name, category, description, index} = currentPhoto;
     return (
         // jsx
@@ -11,9 +11,7 @@ function Modal({currentPhoto}) {
                 <p>
                     {description}
                 </p>
-                <button type="button">
-                    Close this modal
-                </button>
+                <button onClick={onClose} type="button">Close this modal</button>
             </div>
         </div>
     );
--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -127,12 +127,14 @@ const PhotoList = ({ category }) => {
 
   const toggleModal = (image, i) => {
     setCurrentPhoto({...image, index: i })
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
   
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && (
+  <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+)}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
